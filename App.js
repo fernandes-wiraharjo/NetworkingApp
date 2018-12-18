dabcced65513d5ba1819b65edf48fc5df10c6d77
fernandes-wiraharjo <fernandeswiraharjo@gmail.com>
2018-12-18 21:10:08 +0700

    edit app.js

--- App.js
+++ App.js
@@ -14,59 +14,40 @@ class App extends Component {
     };
   }
 
-  openGetPage = () => {
-    this.setState({ pageNeedsTobeRendered: "GetPage" });
-  };
-
-  openPostPage = () => {
-    this.setState({ pageNeedsTobeRendered: "PostPage" });
-  };
-
-  openPutPage = () => {
-    this.setState({ pageNeedsTobeRendered: "PutPage" });
-  };
-
-  openDeletePage = () => {
-    this.setState({ pageNeedsTobeRendered: "DeletePage" });
+  openPage = page => {
+    if (page && page !== "") this.setState({ pageNeedsTobeRendered: page });
   };
 
   render() {
-    const {pageNeedsTobeRendered} = this.state;
+    const { pageNeedsTobeRendered } = this.state;
 
-    if (pageNeedsTobeRendered === "GetPage") {
-      return <GetPage/>;
-    } 
-    else if (pageNeedsTobeRendered === "PostPage") {
-      return <PostPage/>;
-    } 
-    else if (pageNeedsTobeRendered === "PutPage") {
-      return <PutPage/>;
-    } 
-    else if (pageNeedsTobeRendered === "DeletePage") {
-      return <DeletePage/>;
-    } 
+    if (pageNeedsTobeRendered === "Get") {
+      return <GetPage />;
+    } else if (pageNeedsTobeRendered === "Post") {
+      return <PostPage />;
+    } else if (pageNeedsTobeRendered === "Put") {
+      return <PutPage />;
+    } else if (pageNeedsTobeRendered === "Delete") {
+      return <DeletePage />;
+    }
 
     return (
       <View>
         <Text>Networking App</Text>
 
-        <TouchableOpacity
-          onPress={() => this.openGetPage()}>
+        <TouchableOpacity onPress={() => this.openPage("Get")}>
           <Text style={Styles.textButton}>Open Get Page</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity
-          onPress={() => this.openPostPage()}>
+        <TouchableOpacity onPress={() => this.openPostPage("Post")}>
           <Text style={Styles.textButton}>Open Post Page</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity
-          onPress={() => this.openPutPage()}>
+        <TouchableOpacity onPress={() => this.openPutPage("Put")}>
           <Text style={Styles.textButton}>Open Put Page</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity
-          onPress={() => this.openDeletePage()}>
+        <TouchableOpacity onPress={() => this.openDeletePage("Delete")}>
           <Text style={Styles.textButton}>Open Delete Page</Text>
         </TouchableOpacity>
       </View>
